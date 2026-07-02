a28c5b5df5dfa0cf2d3ca096972dca97197f0b6c
tomasgabrielsuarezrojas2005-arch <tsuarezrojas@byupathway.edu>
2026-07-02 11:48:33 -0400

    listisimooooo

---
+++ scripts/getdates.js
@@ -0,0 +1,3 @@
+const year= new Date().getFullYear();
+document.getElementById("currentyear").textContent=year;
+document.getElementById("lastModified").textContent=`Last Modification: ${document.lastModified}`;
